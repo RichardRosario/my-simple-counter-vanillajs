0eac803e300e550810b43bfe4e1acb359455a482
Richard Rosario <tech2js@gmail.com>
2020-06-23 14:08:50 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let count = 0;
+
+const btns = document.querySelectorAll('.btn');
+const value = document.querySelector('#value');
+
+btns.forEach(function(btn){
+    btn.addEventListener('click', function(e){
+       const styles = e.currentTarget.classList;
+       styles.contains('decrease') ? count-- :
+       styles.contains('increase') ? count++ :
+       count = 0;
+
+       count > 0 ? value.style.color = "green" :
+       count < 0 ? value.style.color = "red" :
+       value.style.color = "#000000";
+
+       value.textContent = count;
+    
+    })
+
+});
+
+
