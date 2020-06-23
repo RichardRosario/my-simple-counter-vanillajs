4be44bf107250b4f6b3ddb84d849d0d0ce125ac1
Richard Rosario <tech2js@gmail.com>
2020-06-23 14:13:08 +0800

    refactor code

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 let count = 0;
 
-const btns = document.querySelectorAll('.btn');
-const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+const value = document.querySelector("#value");
 
-btns.forEach(function(btn){
-    btn.addEventListener('click', function(e){
-       const styles = e.currentTarget.classList;
-       styles.contains('decrease') ? count-- :
-       styles.contains('increase') ? count++ :
-       count = 0;
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    styles.contains("decrease")
+      ? count--
+      : styles.contains("increase")
+      ? count++
+      : (count = 0);
+    // refactor to es6 ternary operator
+    count > 0
+      ? (value.style.color = "green")
+      : count < 0
+      ? (value.style.color = "red")
+      : (value.style.color = "#000000");
 
-       count > 0 ? value.style.color = "green" :
-       count < 0 ? value.style.color = "red" :
-       value.style.color = "#000000";
-
-       value.textContent = count;
-    
-    })
-
+    value.textContent = count;
+  });
 });
-
-
